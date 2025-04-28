63c1bb86030c267a7c6009e5cca428b93d6bb4d0
kayky <kaykyrocha97@gmail.com>
2025-04-28 11:18:47 -0300

    test: UpdateRentalServiceTest Should set a pending rental as denied if owner denies it

--- rental/src/main/java/br/ifsp/domain/models/rental/Rental.java
+++ rental/src/main/java/br/ifsp/domain/models/rental/Rental.java
@@ -1,4 +1,12 @@
 package br.ifsp.domain.models.rental;
 
 public class Rental {
+    private RentalState state;
+    public RentalState getState(){
+        return state;
+    }
+    public void setState(RentalState state){
+        this.state=state;
+
+    }
 }

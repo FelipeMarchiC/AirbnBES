9851d62364b885f8dfc0316dbbb537acae1d549e
KaykyRocha016 <kaykyrocha97@gmail.com>
2025-04-29 11:14:33 -0300

    refactoring: UpdateRentalService public void deny() throws exception if RentalState is invalid

--- rental/src/main/java/br/ifsp/application/rental/update/UpdateRentalService.java
+++ rental/src/main/java/br/ifsp/application/rental/update/UpdateRentalService.java
@@ -5,6 +5,9 @@ import br.ifsp.domain.models.rental.RentalState;
 
 public class UpdateRentalService {
     public void deny(Rental rental){
+        if(!rental.getState().equals(RentalState.PENDING) && !rental.getState().equals(RentalState.RESTRAINED)){
+            throw new UnsupportedOperationException(String.format("it´s not possible to deny a rental that is %s",rental.getState().toString()));
+        }
         rental.setState(RentalState.DENIED);
     }
 }

b35be26472eb60162727777e5e972e6b690c94a9
Gabriel <gabriel500henrique@gmail.com>
2025-04-29 10:55:50 -0300

    test: DeleteRentalServiceTest Should return error message when state is not PENDING or DENIED

--- rental/src/main/java/br/ifsp/application/rental/delete/DeleteRentalService.java
+++ rental/src/main/java/br/ifsp/application/rental/delete/DeleteRentalService.java
@@ -3,5 +3,5 @@ package br.ifsp.application.rental.delete;
 import br.ifsp.domain.models.rental.Rental;
 
 public class DeleteRentalService {
-    public void delete(Rental rental){};
+    public void delete(Rental rental){}
 }

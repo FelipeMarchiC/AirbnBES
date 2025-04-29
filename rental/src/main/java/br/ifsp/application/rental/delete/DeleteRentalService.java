c793f6fc2dd86df308dc46c1caf127e62a7566f8
Gabriel <gabriel500henrique@gmail.com>
2025-04-29 11:02:55 -0300

    feat: DeleteRentalService void delete(Rental rental) method created

--- rental/src/main/java/br/ifsp/application/rental/delete/DeleteRentalService.java
+++ rental/src/main/java/br/ifsp/application/rental/delete/DeleteRentalService.java
@@ -1,7 +1,12 @@
 package br.ifsp.application.rental.delete;
 
 import br.ifsp.domain.models.rental.Rental;
+import br.ifsp.domain.models.rental.RentalState;
 
 public class DeleteRentalService {
-    public void delete(Rental rental){}
+    public void delete(Rental rental){
+        if (rental.getState() != RentalState.PENDING && rental.getState() != RentalState.DENIED) {
+            throw new IllegalArgumentException("State must be PENDING or DENIED");
+        }
+    }
 }

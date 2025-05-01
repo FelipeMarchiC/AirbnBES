c1682f8b2828d8be47ab9d855bb2d19178217e37
FelipeMarchiC <felipe.marchi3004@gmail.co>
2025-05-01 13:21:16 -0300

    feat: CreateRentalService initial implementation of register rental

--- rental/src/main/java/br/ifsp/domain/models/rental/Rental.java
+++ rental/src/main/java/br/ifsp/domain/models/rental/Rental.java
@@ -1,10 +1,13 @@
 package br.ifsp.domain.models.rental;
 
+import br.ifsp.domain.models.property.Property;
+import br.ifsp.domain.models.user.User;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.JdbcTypeCode;
 
 import java.sql.Types;
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Data
@@ -20,6 +23,23 @@ public class Rental {
     @NonNull @Column(nullable = false)
     private UUID id;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "property_id", nullable = false)
+    private Property property;
+
+    @NonNull
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @NonNull
+    @Column(nullable = false)
+    private LocalDate endDate;
+
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private RentalState state;
 }

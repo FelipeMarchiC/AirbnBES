e6f252db53b4eec00d49ec881f6f4e1bbf598389
Gabriel-Henrique00 <gabriel500henrique@gmail.com>
2025-05-01 15:44:13 -0300

    refactor: Add query to check for date overlap and rental state in JPARentalRepository

--- rental/src/main/java/br/ifsp/application/rental/repository/JpaRentalRepository.java
+++ rental/src/main/java/br/ifsp/application/rental/repository/JpaRentalRepository.java
@@ -3,6 +3,7 @@ package br.ifsp.application.rental.repository;
 import br.ifsp.domain.models.rental.Rental;
 import br.ifsp.domain.models.rental.RentalState;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
@@ -11,7 +12,15 @@ import java.util.UUID;
 
 public interface JpaRentalRepository extends JpaRepository<Rental, UUID> {
     List<Rental> findByPropertyId(UUID propertyId);
+
     List<Rental> findByTenantId(UUID tenantId);
+
+    @Query("SELECT r FROM Rental r WHERE r.property.id = :propertyId " +
+            "AND r.state = :state " +
+            "AND ((r.startDate BETWEEN :startDate AND :endDate) " +
+            "OR (r.endDate BETWEEN :startDate AND :endDate) " +
+            "OR (r.startDate <= :startDate AND r.endDate >= :endDate)) " +
+            "AND r.id != :rentalId")
     List<Rental> findRentalsByOverlapAndState(
             @Param("propertyId") UUID propertyId,
             @Param("state") RentalState state,
@@ -20,4 +29,3 @@ public interface JpaRentalRepository extends JpaRepository<Rental, UUID> {
             @Param("rentalId") UUID rentalId
     );
 }
-

11ab743c86e2dac7b4cd4eac2d68ce35d899697c
Gabriel-Henrique00 <gabriel500henrique@gmail.com>
2025-05-03 14:18:04 -0300

    refactor: update RentalController to use PUT for state-changing operations

--- rental/src/main/java/br/ifsp/application/property/JpaPropertyRepository.java
+++ rental/src/main/java/br/ifsp/application/property/JpaPropertyRepository.java
@@ -2,8 +2,12 @@ package br.ifsp.application.property;
 
 import br.ifsp.domain.models.property.Property;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface JpaPropertyRepository extends JpaRepository<Property, UUID> {
+    @Query("SELECT p FROM Property p WHERE p.address.city = :location")
+    List<Property> findByLocation(String location);
 }

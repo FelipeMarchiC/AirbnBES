e5ed74b915d765ebbf1c558cf29fec8ac4be540e
KaykyRocha016 <kaykyrocha97@gmail.com>
2025-05-04 18:01:00 -0300

    feat: JpaPropertyRepository findAvailablePropertiesByPeriod method added

--- rental/src/main/java/br/ifsp/application/property/JpaPropertyRepository.java
+++ rental/src/main/java/br/ifsp/application/property/JpaPropertyRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -13,4 +14,6 @@ public interface JpaPropertyRepository extends JpaRepository<Property, UUID> {
     List<Property> findByLocation(String location);
     @Query("SELECT p FROM Property p WHERE p.dailyRate.amount BETWEEN :min AND :max")
     List<Property> findByDailyRateBetween(@Param("min") double min, @Param("max") double max);
+    @Query("SELECT p FROM Property p WHERE p.id NOT IN (SELECT r.property.id FROM Rental r WHERE r.state = :state AND ((r.startDate BETWEEN :startDate AND :endDate) OR (r.endDate BETWEEN :startDate AND :endDate) OR (r.startDate <= :startDate AND r.endDate >= :endDate)))")
+    List<Property> findAvailablePropertiesByPeriod(LocalDate startDate, LocalDate endDate);
 }

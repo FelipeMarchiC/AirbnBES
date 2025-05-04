55e8a3cfc89dc34bcfde4a61de736b657b1accb3
Gabriel-Henrique00 <gabriel500henrique@gmail.com>
2025-05-04 01:29:56 -0300

    feat: JpaPropertyRepository add method List<Property> findByDailyRateBetween

--- rental/src/main/java/br/ifsp/application/property/JpaPropertyRepository.java
+++ rental/src/main/java/br/ifsp/application/property/JpaPropertyRepository.java
@@ -3,6 +3,7 @@ package br.ifsp.application.property;
 import br.ifsp.domain.models.property.Property;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
@@ -10,4 +11,6 @@ import java.util.UUID;
 public interface JpaPropertyRepository extends JpaRepository<Property, UUID> {
     @Query("SELECT p FROM Property p WHERE p.address.city = :location")
     List<Property> findByLocation(String location);
+    @Query("SELECT p FROM Property p WHERE p.dailyRate.amount BETWEEN :min AND :max")
+    List<Property> findByDailyRateBetween(@Param("min") double min, @Param("max") double max);
 }

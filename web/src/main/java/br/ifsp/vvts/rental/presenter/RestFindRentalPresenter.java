850c7aa5ecd22198ef52b763d1f123eb980d371e
KaykyRocha016 <kaykyrocha97@gmail.com>
2025-05-04 23:12:05 -0300

    Feat: RentalController @GetMapping("/{propertyId}") created

---
+++ web/src/main/java/br/ifsp/vvts/rental/presenter/RestFindRentalPresenter.java
@@ -0,0 +1,71 @@
+package br.ifsp.vvts.rental.presenter;
+
+import br.ifsp.application.rental.find.FindRentalPresenter;
+import br.ifsp.application.rental.find.IFindRentalService;
+import br.ifsp.application.rental.update.owner.IOwnerUpdateRentalService;
+import br.ifsp.domain.models.rental.Rental;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+import static br.ifsp.vvts.shared.error.ErrorResponseFactory.createErrorResponseFrom;
+
+public class RestFindRentalPresenter implements FindRentalPresenter {
+    private ResponseEntity<?> responseEntity;
+
+    private List<UUID> rentalIdList;
+    private List<String> userNameList;
+    private List<String> propertyNameList;
+    private List<LocalDate> startDateList;
+    private List<LocalDate> endDateList;
+    private List<Double> prices;
+
+    @Override
+    public void prepareSuccessView(IFindRentalService.ResponseModel response) {
+        response.rentalList().forEach(rental -> rentalIdList.add(rental.getId()));
+        response.rentalList().forEach(rental -> userNameList.add(rental.getUser().getUsername()));
+        response.rentalList().forEach(rental -> propertyNameList.add(rental.getProperty().getName()));
+        response.rentalList().forEach(rental -> startDateList.add(rental.getStartDate()));
+        response.rentalList().forEach(rental -> endDateList.add(rental.getEndDate()));
+        response.rentalList().forEach(rental -> prices.add(rental.getValue().getAmount().doubleValue()));
+
+        RestFindRentalPresenter.ViewModel viewModel = new RestFindRentalPresenter.ViewModel(
+                rentalIdList,
+                userNameList,
+                propertyNameList,
+                startDateList,
+                endDateList,
+                prices
+        );
+
+        this.responseEntity = ResponseEntity.status(HttpStatus.CREATED).body(viewModel);
+    }
+
+    @Override
+    public void prepareFailView(Throwable throwable) {
+        this.responseEntity = createErrorResponseFrom(throwable);
+    }
+
+    @Override
+    public boolean isDone() {
+        return responseEntity != null;
+    }
+
+    public ResponseEntity<?> responseEntity() {
+        return responseEntity;
+    }
+
+    private record ViewModel(
+            List<UUID> rentalIdList,
+
+            List<String> userNameList,
+            List<String> propertyNameList,
+            List<LocalDate> startDateList,
+            List<LocalDate> endDateList,
+            List<Double> prices
+
+    ) {}
+}

8880e474a8838c18544c75a2b4c1ff098ffbfd2c
Gabriel-Henrique00 <gabriel500henrique@gmail.com>
2025-05-04 23:54:23 -0300

    refactor: ViewModel to show only relevant property details

--- web/src/main/java/br/ifsp/vvts/property/presenter/RestFindPropertyPresenter.java
+++ web/src/main/java/br/ifsp/vvts/property/presenter/RestFindPropertyPresenter.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static br.ifsp.vvts.shared.error.ErrorResponseFactory.createErrorResponseFrom;
 
@@ -15,7 +16,30 @@ public class RestFindPropertyPresenter implements FindPropertyPresenter {
 
     @Override
     public void prepareSuccessView(IFindPropertyService.PropertyListResponseModel response) {
-        ViewModel viewModel = new ViewModel(response.properties());
+        List<Property> properties = response.properties();
+
+        List<String> propertyNames = properties.stream()
+                .map(Property::getName)
+                .collect(Collectors.toList());
+
+        List<String> propertyDescriptions = properties.stream()
+                .map(Property::getDescription)
+                .collect(Collectors.toList());
+
+        List<Double> propertyDailyRates = properties.stream()
+                .map(property -> property.getDailyRate().getAmount().doubleValue())
+                .collect(Collectors.toList());
+
+        List<String> ownerNames = properties.stream()
+                .map(property -> property.getOwner().getUsername())
+                .collect(Collectors.toList());
+
+        ViewModel viewModel = new ViewModel(
+                propertyNames,
+                propertyDescriptions,
+                propertyDailyRates,
+                ownerNames
+        );
 
         this.responseEntity = ResponseEntity.status(HttpStatus.CREATED).body(viewModel);
     }
@@ -30,7 +54,14 @@ public class RestFindPropertyPresenter implements FindPropertyPresenter {
         return responseEntity != null;
     }
 
-    public ResponseEntity<?> responseEntity() { return responseEntity; }
+    public ResponseEntity<?> responseEntity() {
+        return responseEntity;
+    }
 
-    private record ViewModel(List<Property> properties) {}
+    private record ViewModel(
+            List<String> propertyNames,
+            List<String> propertyDescriptions,
+            List<Double> propertyDailyRates,
+            List<String> ownerNames
+    ) {}
 }

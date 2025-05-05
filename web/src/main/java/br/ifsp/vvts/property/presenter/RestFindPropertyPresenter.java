a82d71b7d505cb81f71fe19128bc2a23b46a7b02
Gabriel-Henrique00 <gabriel500henrique@gmail.com>
2025-05-05 00:03:41 -0300

    feat: return properties as structured JSON objects

--- web/src/main/java/br/ifsp/vvts/property/presenter/RestFindPropertyPresenter.java
+++ web/src/main/java/br/ifsp/vvts/property/presenter/RestFindPropertyPresenter.java
@@ -2,12 +2,10 @@ package br.ifsp.vvts.property.presenter;
 
 import br.ifsp.application.property.find.FindPropertyPresenter;
 import br.ifsp.application.property.find.IFindPropertyService;
-import br.ifsp.domain.models.property.Property;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static br.ifsp.vvts.shared.error.ErrorResponseFactory.createErrorResponseFrom;
 
@@ -16,32 +14,16 @@ public class RestFindPropertyPresenter implements FindPropertyPresenter {
 
     @Override
     public void prepareSuccessView(IFindPropertyService.PropertyListResponseModel response) {
-        List<Property> properties = response.properties();
+        List<PropertyViewModel> viewModelList = response.properties().stream()
+                .map(property -> new PropertyViewModel(
+                        property.getName(),
+                        property.getDescription(),
+                        property.getDailyRate().getAmount().doubleValue(),
+                        property.getOwner().getUsername()
+                ))
+                .toList();
 
-        List<String> propertyNames = properties.stream()
-                .map(Property::getName)
-                .collect(Collectors.toList());
-
-        List<String> propertyDescriptions = properties.stream()
-                .map(Property::getDescription)
-                .collect(Collectors.toList());
-
-        List<Double> propertyDailyRates = properties.stream()
-                .map(property -> property.getDailyRate().getAmount().doubleValue())
-                .collect(Collectors.toList());
-
-        List<String> ownerNames = properties.stream()
-                .map(property -> property.getOwner().getUsername())
-                .collect(Collectors.toList());
-
-        ViewModel viewModel = new ViewModel(
-                propertyNames,
-                propertyDescriptions,
-                propertyDailyRates,
-                ownerNames
-        );
-
-        this.responseEntity = ResponseEntity.status(HttpStatus.CREATED).body(viewModel);
+        this.responseEntity = ResponseEntity.status(HttpStatus.OK).body(viewModelList);
     }
 
     @Override
@@ -58,10 +40,10 @@ public class RestFindPropertyPresenter implements FindPropertyPresenter {
         return responseEntity;
     }
 
-    private record ViewModel(
-            List<String> propertyNames,
-            List<String> propertyDescriptions,
-            List<Double> propertyDailyRates,
-            List<String> ownerNames
+    private record PropertyViewModel(
+            String name,
+            String description,
+            Double dailyRate,
+            String owner
     ) {}
 }

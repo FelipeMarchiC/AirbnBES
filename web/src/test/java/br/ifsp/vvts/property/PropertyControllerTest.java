02461134889bb7885380e00ad0c6a687e0ddd387
gcontiero11 <gustcontiero@gmail.com>
2025-06-20 19:13:01 -0300

    test: Should return properties by the city

--- web/src/test/java/br/ifsp/vvts/property/PropertyControllerTest.java
+++ web/src/test/java/br/ifsp/vvts/property/PropertyControllerTest.java
@@ -313,5 +313,27 @@ public class PropertyControllerTest extends BaseApiIntegrationTest {
 
             assertThat(response.jsonPath().getList("id")).contains(p1.getId().toString());
         }
+
+        @Test
+        @Tag("ApiTest")
+        @Tag("IntegrationTest")
+        @DisplayName("Should return properties by the city")
+        void shouldReturnPropertiesByTheCity() {
+            String token = authenticate(user.getEmail(), userPassword);
+            Address l1 = Address.builder()
+                    .number(faker.address().buildingNumber())
+                    .street(faker.address().streetName())
+                    .city(faker.address().city())
+                    .state(faker.address().stateAbbr())
+                    .postalCode(faker.address().zipCode())
+                    .build();
+
+            PropertyEntity p1 = EntityBuilder.createPropertyWithLocation(user, l1);
+            propertyRepository.save(p1);
+
+            Response response = sendRequest(l1.getCity(), token);
+
+            assertThat(response.jsonPath().getList("id")).contains(p1.getId().toString());
+        }
     }
 }

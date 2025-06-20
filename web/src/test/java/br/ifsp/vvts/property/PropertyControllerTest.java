0d2fa1d1b5b1e1e370f1e65eb4dce91b1f667369
gcontiero11 <gustcontiero@gmail.com>
2025-06-20 17:40:46 -0300

    test: testing the endpoint that returns a list of all properties the user

--- web/src/test/java/br/ifsp/vvts/property/PropertyControllerTest.java
+++ web/src/test/java/br/ifsp/vvts/property/PropertyControllerTest.java
@@ -4,21 +4,32 @@ import br.ifsp.application.property.repository.JpaPropertyRepository;
 import br.ifsp.application.user.repository.JpaUserRepository;
 import br.ifsp.domain.models.property.PropertyEntity;
 import br.ifsp.domain.models.user.Role;
+import br.ifsp.domain.models.user.User;
 import br.ifsp.domain.models.user.UserEntity;
 import br.ifsp.domain.shared.valueobjects.Address;
 import br.ifsp.domain.shared.valueobjects.Price;
 import br.ifsp.vvts.utils.BaseApiIntegrationTest;
+import br.ifsp.vvts.utils.EntityBuilder;
 import com.github.javafaker.Faker;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import lombok.RequiredArgsConstructor;
+import org.apache.maven.surefire.shared.utils.cli.Arg;
+import org.apache.maven.surefire.shared.utils.cli.Commandline;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
 
+import static io.restassured.RestAssured.authentication;
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RequiredArgsConstructor
 public class PropertyControllerTest extends BaseApiIntegrationTest {
@@ -31,7 +42,7 @@ public class PropertyControllerTest extends BaseApiIntegrationTest {
     @Autowired
     private JpaUserRepository jpaUserRepository;
 
-    @BeforeAll
+    @BeforeEach
     void setUp() {
         userPassword = faker.internet().password();
         adminPassword = faker.internet().password();
@@ -39,59 +50,99 @@ public class PropertyControllerTest extends BaseApiIntegrationTest {
         admin = registerAdminUser(adminPassword);
     }
 
-    @Test
-    @Tag("ApiTest")
-    @Tag("IntegrationTest")
-    @DisplayName("Should list all properties to user")
-    void listAllPropertiesForRegularUser() {
-        PropertyEntity property = PropertyEntity.builder()
-                .id(UUID.randomUUID())
-                .name("Casa na praia em riviera")
-                .owner(user)
-                .dailyRate(new Price(BigDecimal.valueOf(590)))
-                .description("Bela casa")
-                .address(Address.builder()
-                        .state("São Paulo")
-                        .city("Riviera")
-                        .postalCode("Ai ta querendo dms")
-                        .number("69")
-                        .street("Rua Osvaldo")
-                        .build()
-                ).build();
-    }
+    @Nested
+    class ListAllProperties {
 
-    @Test
-    @Tag("ApiTest")
-    @Tag("IntegrationTest")
-    @DisplayName("Should list all properties to admin")
-    void listAllPropertiesForAdmin() {
-        String token = authenticate(admin.getEmail(), adminPassword);
+        private Response sendListAllPropertiesRequest(String token) {
+            return given()
+                    .contentType(ContentType.JSON)
+                    .port(port)
+                    .header("Authorization", "Bearer " + token)
+                    .when().get("/api/v1/property")
+                    .then()
+                    .log().all()
+                    .extract().response();
+        }
 
-        PropertyEntity property = PropertyEntity.builder()
-                .id(UUID.randomUUID())
-                .name("Casa na praia em riviera")
-                .owner(admin)
-                .dailyRate(new Price(BigDecimal.valueOf(590)))
-                .description("Bela casa")
-                .address(Address.builder()
-                        .state("São Paulo")
-                        .city("Riviera")
-                        .postalCode("Ai ta querendo dms")
-                        .number("69")
-                        .street("Rua Osvaldo")
-                        .build()
-                ).build();
+        @Test
+        @Tag("ApiTest")
+        @Tag("IntegrationTest")
+        @DisplayName("Should list all properties to user")
+        void listAllPropertiesForRegularUser() {
+            String token = authenticate(user.getEmail(), userPassword);
+            PropertyEntity property = EntityBuilder.createRandomProperty(user);
+            propertyRepository.save(property);
 
-        var response = given()
-                .contentType(ContentType.JSON)
-                .port(port)
-                .header("Authorization", "Bearer " + token)
-                .when().get("/api/v1/property")
-                .then()
-                .log().all()
-                .statusCode(200)
-                .extract().response();
+            Response response = sendListAllPropertiesRequest(token);
 
-        System.out.println(response);
+            assertThat(response.getBody().jsonPath().getList("id")).contains(property.getId().toString());
+        }
+
+        @Test
+        @Tag("ApiTest")
+        @Tag("IntegrationTest")
+        @DisplayName("Should list all properties to admin")
+        void listAllPropertiesForAdmin() {
+            String token = authenticate(admin.getEmail(), adminPassword);
+
+            PropertyEntity property = EntityBuilder.createRandomProperty(admin);
+            propertyRepository.save(property);
+
+            Response response = sendListAllPropertiesRequest(token);
+
+            assertThat(response.getBody().jsonPath().getList("id")).contains(property.getId().toString());
+        }
+
+        @Test
+        @Tag("ApiTest")
+        @Tag("IntegrationTest")
+        @DisplayName("Should return empty list when no properties are registered")
+        void listAllPropertiesWhenNoPropertiesAreRegistered() {
+            String token = authenticate(admin.getEmail(), adminPassword);
+
+            Response response = sendListAllPropertiesRequest(token);
+
+            assertThat(response.getBody().jsonPath().getList("id")).isEmpty();
+        }
+
+        @Test
+        @Tag("ApiTest")
+        @Tag("IntegrationTest")
+        @DisplayName("Should return forbidden (401) when user is not authorized")
+        void listAllPropertiesWhenUserIsNotAuthorized() {
+            String token = faker.rickAndMorty().character();
+
+            Response response = sendListAllPropertiesRequest(token);
+
+            assertThat(response.getStatusCode()).isEqualTo(401);
+        }
+
+        @Test
+        @Tag("ApiTest")
+        @Tag("IntegrationTest")
+        @DisplayName("Should return OK (200) if the list is empty")
+        void shouldReturnOK200IfTheListIsEmpty() {
+            String token = authenticate(user.getEmail(), userPassword);
+
+            Response response = sendListAllPropertiesRequest(token);
+
+            assertThat(response.getStatusCode()).isEqualTo(200);
+        }
+
+        @Test
+        @Tag("ApiTest")
+        @Tag("IntegrationTest")
+        @DisplayName("Should return OK (200) if the list is not empty")
+        void shouldReturnOK200IfTheListIsNotEmpty() {
+            String token = authenticate(user.getEmail(), userPassword);
+
+            PropertyEntity property = EntityBuilder.createRandomProperty(user);
+            propertyRepository.save(property);
+
+            Response response = sendListAllPropertiesRequest(token);
+
+            assertThat(response.getStatusCode()).isEqualTo(200);
+        }
+
     }
 }

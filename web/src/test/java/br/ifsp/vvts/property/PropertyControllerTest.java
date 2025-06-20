b65c5dc802def1c3fee7d86e4cac904b4c00264f
gcontiero11 <gustcontiero@gmail.com>
2025-06-20 19:09:56 -0300

    test: Should return properties by the street

--- web/src/test/java/br/ifsp/vvts/property/PropertyControllerTest.java
+++ web/src/test/java/br/ifsp/vvts/property/PropertyControllerTest.java
@@ -297,5 +297,27 @@ public class PropertyControllerTest extends BaseApiIntegrationTest {
 
             assertThat(response.jsonPath().getList("id")).contains(p1.getId().toString());
         }
+
+        @Test
+        @Tag("ApiTest")
+        @Tag("IntegrationTest")
+        @DisplayName("Should return properties by the street")
+        void shouldReturnPropertiesByTheStreet() {
+            String token = authenticate(user.getEmail(), userPassword);
+            Address l1 = Address.builder()
+                    .number(faker.address().buildingNumber())
+                    .street(faker.address().streetName())
+                    .city(faker.address().city())
+                    .state(faker.address().stateAbbr())
+                    .postalCode(faker.address().zipCode())
+                    .build();
+
+            PropertyEntity p1 = EntityBuilder.createPropertyWithLocation(user, l1);
+            propertyRepository.save(p1);
+
+            Response response = sendRequest(l1.getStreet(), token);
+
+            assertThat(response.jsonPath().getList("id")).contains(p1.getId().toString());
+        }
     }
 }

109c8d74a529381c6c70706af61589163d4c1495
brunomascioli <souzamasciolibruno@gmail.com>
2025-06-22 15:28:03 -0300

    test(ui): add should login user with valid credentials.

--- web/src/test/java/br/ifsp/vvts/ui/test/LoginPageTest.java
+++ web/src/test/java/br/ifsp/vvts/ui/test/LoginPageTest.java
@@ -1,14 +1,21 @@
 package br.ifsp.vvts.ui.test;
 
 import br.ifsp.application.user.repository.JpaUserRepository;
+import br.ifsp.domain.models.user.UserEntity;
 import br.ifsp.vvts.ui.BaseSeleniumTest;
 import br.ifsp.vvts.ui.page.LoginPageObject;
+import br.ifsp.vvts.utils.EntityBuilder;
 import com.github.javafaker.Faker;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.pitest.reloc.antlr.common.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -32,5 +39,22 @@ public class LoginPageTest extends BaseSeleniumTest {
     @AfterEach
     void cleanUp() {
         userRepository.deleteAll();
+    }
+
+    @Test
+    @Tag("UiTest")
+    void shouldLoginUserWithValidCredentials() {
+        String password = "validPassword123!";
+        UserEntity user = EntityBuilder.createRandomUser(password);
+        String email = user.getEmail();
+
+        userRepository.save(user);
+
+        loginPageObject.loginUser(email, password);
+
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.urlContains("/"));
+
+        assertThat(driver.getCurrentUrl()).contains("/");
     }
 }

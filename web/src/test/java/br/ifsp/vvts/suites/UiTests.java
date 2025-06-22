7d56ef104e13e8b60416aae8a59f2939414c198a
brunomascioli <souzamasciolibruno@gmail.com>
2025-06-21 23:01:33 -0300

    test(ui): add Ui test suite.

---
+++ web/src/test/java/br/ifsp/vvts/suites/UiTests.java
@@ -0,0 +1,13 @@
+package br.ifsp.vvts.suites;
+
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SelectPackages({"br.ifsp.vvts.ui"})
+@SuiteDisplayName("All Ui tests")
+@IncludeTags({"UiTest"})
+public class UiTests {
+}

afee60efead5cb57b83275f031adedb669c1b1ae
brunomascioli <souzamasciolibruno@gmail.com>
2025-06-22 20:33:02 -0300

    test(ui): should render manage rentals page when admin is authenticated.

--- web/src/test/java/br/ifsp/vvts/ui/page/DashboardPageObject.java
+++ web/src/test/java/br/ifsp/vvts/ui/page/DashboardPageObject.java
@@ -3,10 +3,26 @@ package br.ifsp.vvts.ui.page;
 import br.ifsp.vvts.ui.BasePageObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class DashboardPageObject extends BasePageObject {
     private final By manageRentalsButton = By.xpath("//*[@id=\"root\"]/div[1]/aside/nav/a[2]");
+    private final By dashboardButton = By.xpath("//*[@id=\"root\"]/div[1]/aside/nav/a[1]");
+    private final By linkToMainPage = By.xpath("//*[@id=\"root\"]/div[1]/aside/nav/a[3]");
+
     public DashboardPageObject(WebDriver driver) {
         super(driver);
+    }
+
+    public void clickDashboardButton() {
+        wait.until(ExpectedConditions.elementToBeClickable(dashboardButton)).click();
+    }
+
+    public void clickManageRentalsButton() {
+        wait.until(ExpectedConditions.elementToBeClickable(manageRentalsButton)).click();
+    }
+
+    public void clickLinkToMainPage() {
+        wait.until(ExpectedConditions.elementToBeClickable(linkToMainPage)).click();
     }
 }
--- web/src/test/java/br/ifsp/vvts/ui/test/DashboardPageTest.java
+++ web/src/test/java/br/ifsp/vvts/ui/test/DashboardPageTest.java
@@ -1,15 +1,21 @@
 package br.ifsp.vvts.ui.test;
 
 import br.ifsp.vvts.ui.BaseSeleniumTest;
+import br.ifsp.vvts.ui.page.DashboardPageObject;
 import br.ifsp.vvts.ui.page.LoginPageObject;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class DashboardPageTest extends BaseSeleniumTest {
+    private DashboardPageObject dashboardPageObject;
 
     @Override
     protected void setInitialPage() {
@@ -19,7 +25,9 @@ public class DashboardPageTest extends BaseSeleniumTest {
     @BeforeEach
     void setUpPage() {
         var loginPageObject = new LoginPageObject(driver);
-        loginPageObject.loginUser(admin.getEmail(), admin.getPassword());
+        loginPageObject.loginUser(admin.getEmail(), usersPassword);
+
+        this.dashboardPageObject = new DashboardPageObject(driver);
     }
 
     @AfterEach
@@ -27,4 +35,16 @@ public class DashboardPageTest extends BaseSeleniumTest {
         userRepository.deleteAll();
     }
 
+    @Nested
+    class ManageRentals {
+        @Test
+        @Tag("UiTest")
+        @DisplayName("Should render manage rentals page")
+        void shouldRenderManageRentalsPage() {
+            dashboardPageObject.clickManageRentalsButton();
+            new WebDriverWait(driver, Duration.ofSeconds(10))
+                    .until(ExpectedConditions.urlContains("/admin/alugueis"));
+            assertThat(driver.getCurrentUrl()).contains("/admin/alugueis");
+        }
+    }
 }
